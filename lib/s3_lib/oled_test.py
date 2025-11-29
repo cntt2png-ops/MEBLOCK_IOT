1d6e72b7c27e55b90bb04655341c314d11609037
fanmeanthong <thong.phan0601@gmail.com>
2025-11-29 10:09:23 +0700

    Update

--- lib/s3_lib/oled_test.py
+++ lib/s3_lib/oled_test.py
@@ -1,14 +1,7 @@
 from oled import Oled
 import time
-oled = Oled(scl=5, sda=4, width=128, height=64)
+oled = Oled(scl=9, sda=8, width=128, height=64)
 
 oled.clear()
-oled.dev.text("Hello, S3!", 0, 0, 1)
+oled.text("Hello, S3!",64, 1, 1)
 oled.show()
-i=0
-while True:
-    oled.clear()
-    oled.dev.text("OLED Test " + str(i), 0, 16, 1)
-    oled.show()
-    i += 1
-    time.sleep(1)
